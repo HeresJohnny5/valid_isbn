e17f51c0bb705328a8b1c70c5ae78a18ab81de38
John Erickson <ericksonjohn08@hotmail.com>
2016-08-29 14:33:31 -0400

    Added tests converting string to array and checking for dash or space.

--- isbn_test.rb
+++ isbn_test.rb
@@ -6,14 +6,43 @@
 require_relative "isbn.rb"
 require "minitest/autorun"
 
-class ValidIsbn < Minitest::Test
+class TestValidIsbn < Minitest::Test
 
 	def test_empty_string_returns_false()
-		assert_equal(false, valid_isbn?(""))
+		assert_equal(false, valid_isbn_length?(""))
 	end
 
-	# def test_enters_string_returns_false()
-	# 	assert_equal(false, valid_isbn?(""))
-	# end
+	def test_valid_ten_digit_isbn_string_returns_true()
+		assert_equal(true, valid_isbn_length?("0471958697"))
+	end
+
+	def test_invalid_ten_digit_isbn_string_returns_false()
+		skip("skip")
+		assert_equal(false, valid_isbn_length?("0471958699"))
+	end
+
+end
+
+class TestConvertsStringToArray < Minitest::Test
+
+	def test_string_returns_empty_array
+		assert_equal([], string_to_array(""))
+	end
+
+	def test_12345_string_returns_array_with_12345
+		assert_equal(["1", "2", "3", "4", "5"], string_to_array("12345"))
+	end
+
+end
+
+class TestToCheckForDashOrSpace < Minitest::Test
+
+	def test_string_with_space_returns_string_without_space
+		assert_equal("12345", string_removes_space_or_dash("123 45"))
+	end
+
+	def test_string_with_dash_returns_string_without_dash
+		assert_equal("12345", string_removes_space_or_dash("123-45"))
+	end
 
 end
